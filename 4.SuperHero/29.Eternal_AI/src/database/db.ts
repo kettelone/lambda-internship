06d9d8b3807394a42bb0ddf9e5e409b67552f17c
Java Babykish <bkrevsun@gmail.com>
2023-08-08 03:16:17 +0400

    signup implementation

--- 4.SuperHero/29.Eternal_AI/src/database/db.ts
+++ 4.SuperHero/29.Eternal_AI/src/database/db.ts
@@ -23,15 +23,10 @@ import 'dotenv/config';
 
 import { neon, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import { migrate } from 'drizzle-orm/neon-http/migrator';
 
 neonConfig.fetchConnectionCache = true;
 
 const sql = neon(process.env.DB_CONNECTION_STRING!);
 const db = drizzle(sql);
-const runMigration = async () => {
-  await migrate(db, { migrationsFolder: './drizzle' });
-};
-runMigration();
 
 export default db;
